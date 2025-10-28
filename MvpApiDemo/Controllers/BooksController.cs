1ecdfcb8fba9fed763ea0406583fe075741e6c9d
nzandy <andydaviesnz@gmail.com>
2025-10-28 13:57:32 +0000

    Add new books controller + book model

---
+++ MvpApiDemo/Controllers/BooksController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcApiDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        [HttpPost]
+        public void AddBook()
+        {
+
+        }
+    }
+}
---
+++ MvpApiDemo/Models/Book.cs
@@ -0,0 +1,8 @@
+﻿namespace MvpApiDemo.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public required string Title { get; set; }
+    }
+}
